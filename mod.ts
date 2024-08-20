89b85528bf265722901e651180a1bf6b5f31d160
wrnrlr <werner@alman.ax>
2024-08-20 06:20:55 +0200

    Add nbsp

--- mod.ts
+++ mod.ts
@@ -2,29 +2,8 @@ export {signal,effect,sample} from './signal.ts'
 import {runtime} from './runtime.js'
 import {hyperscript} from './hyperscript.js'
 
-// export const root = S.root
-// export const sample = S.sample
-// export const effect = S
-// export const signal = S.value
-
-// import {Signal} from './s2.js'
-
-// export class ValueSignal extends Signal {
-//   constructor(_) {
-//     super(_)
-//     return new Proxy(this,{apply:(signal,_,args) => {
-//       if (args.length===0) return signal.value
-//     }})
-//   }
-// }
-
-// export function signal2(value) {
-//   const s = new Signal(value)
-//   return new Proxy(s,{apply:(s,_,args)=>{}})
-// }
-
 const r = runtime(window), h = hyperscript(r), render = r.render
 
-const e0 = 1e1
+const nbsp = '\u00A0'
 
-export {h,render,runtime,hyperscript,e0}
+export {h,render,runtime,hyperscript,nbsp}
