8ebe8cf4217e01737affb6a132243bf45ace8895
wrnrlr <werner@alman.ax>
2024-08-21 05:46:15 +0200

    Add onCleanup

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-export {signal,effect,sample} from './signal.ts'
+import {signal,effect,sample} from './signal.ts'
 import {runtime} from './runtime.js'
 import {hyperscript} from './hyperscript.js'
 
@@ -6,4 +6,8 @@ const r = runtime(window), h = hyperscript(r), render = r.render
 
 const nbsp = '\u00A0'
 
-export {h,render,runtime,hyperscript,nbsp}
+function onMount(fn: () => void) {
+  effect(() => sample(fn));
+}
+
+export {signal,effect,sample,h,render,runtime,hyperscript,onMount,nbsp}
