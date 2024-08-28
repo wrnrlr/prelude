9e865c535374e284426b75c4235f9ef98b0a5b3b
wrnrlr <werner@alman.ax>
2024-08-29 00:40:02 +0200

    reorganize exports

--- src/mod.ts
+++ src/mod.ts
@@ -1,13 +1,9 @@
-import {signal,computed,effect,sample} from './signal.ts'
-import {runtime} from './runtime.js'
-import {hyperscript} from './hyperscript.js'
+export {signal,computed,effect,sample,onMount} from './signal.ts'
+export {nbsp} from './constants.ts'
+
+import {runtime} from './runtime.ts'
+import {hyperscript} from './hyperscript.ts'
 
 const r = runtime(window), h = hyperscript(r), render = r.render
 
-const nbsp = '\u00A0'
-
-function onMount(fn: () => void) {
-  effect(() => sample(fn));
-}
-
-export {signal,computed,effect,sample,h,render,runtime,hyperscript,onMount,nbsp}
+export {h,render}
