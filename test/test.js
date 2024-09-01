0351beca6356d24af226f9cfb48b462823b1e223
wrnrlr <werner@alman.ax>
2024-09-01 23:45:36 +0200

    move test.js to test folder

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 import {assertEquals,assert} from '@std/assert'
 import {describe,it} from '@std/testing/bdd'
 
-import {signal,computed,effect,sample,batch} from './signal.ts'
+import {signal,computed,effect,sample,batch} from '../src/mod.ts'
 
 describe('signal',()=>{
   const a = signal(1)
@@ -62,14 +62,14 @@ describe('batch',()=>{
   assertEquals(i,2)
 })
 
-import {runtime} from './runtime.ts'
-import {hyperscript} from './hyperscript.ts'
+import {runtime} from '../src/runtime.ts'
+import {hyperscript} from '../src/hyperscript.ts'
 import {JSDOM} from 'npm:jsdom'
 
 describe("hyperscript", () => {
   const FIXTURES = [
     '<div id="main"><h1>Hi</h1><span style="color: rgb(85, 85, 85);">555</span><label for="entry" class="name">Edit:</label><input id="entry" type="text" readonly=""></div>',
-    '<div id="main" refset="true" class="selected"><h1 title="hello" style="background-color: red;"><a href="/">Welcome</a></h1></div>',
+    '<div id="main" refset="true" class="selected"><h1 title="hello" style="background-color: red;"><a href="/daft/public">Welcome</a></h1></div>',
     '<div id="main"><button>Click Bound</button><button>Click Delegated</button><button>Click Listener</button></div>',
     '<div>First</div>middle<div>Last</div>',
     '<div id="main"><div>John R.<span>Smith</span></div></div>',
@@ -102,6 +102,14 @@ describe("hyperscript", () => {
 
   describe("Component without props or children", () => {
     assertEquals(h('hr')().outerHTML,`<hr>`)
+  });
+
+  describe("Element with single child ", () => {
+    const name = signal('Bob')
+    assertEquals(h('b',[name])().outerHTML,`<b>Bob</b>`)
+    assertEquals(h('b',[()=>name])().outerHTML,`<b>Bob</b>`)
+    assertEquals(h('b',name())().outerHTML,`<b>Bob</b>`)
+    // assertEquals(h('b',()=>name())().outerHTML,`<b>Bob</b>`)
   });
 
   // describe("Custom Component", () => {
