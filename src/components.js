15fed9532ff0568bb27c48a3a4cbe52e52e255b2
wrnrlr <werner@alman.ax>
2024-09-22 21:23:55 +0200

    clean file

--- src/components.js
+++ src/components.js
@@ -1,23 +1,20 @@
 /**
+TODO
 @group Components
 */
 export function Input(props) {
-  const fallback = "fallback" in props && { fallback: () => props.fallback }
-  return memo(listArray(() => props.each, props.children, fallback || undefined))
 }
 
 /**
+TODO
 @group Components
 */
 export function Table(props) {
-  const fallback = "fallback" in props && { fallback: () => props.fallback }
-  return memo(listArray(() => props.each, props.children, fallback || undefined))
 }
 
 /**
+TODO
 @group Components
 */
 export function Canvas(props) {
-  const fallback = "fallback" in props && { fallback: () => props.fallback }
-  return memo(listArray(() => props.each, props.children, fallback || undefined))
 }
