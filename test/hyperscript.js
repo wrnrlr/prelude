7f3efbf2c6bfef00def2fc339ff323617bdfacf9
wrnrlr <werner@alman.ax>
2024-10-07 00:21:39 +0200

    Refactor hyperscript tests

--- test/hyperscript.js
+++ test/hyperscript.js
@@ -1,106 +1,84 @@
 import {runtime} from '../src/runtime.ts'
 import {hyperscript} from '../src/hyperscript.ts'
+import {signal,root} from '../src/signal.ts'
 import {JSDOM} from 'npm:jsdom'
+import {assertEquals} from '@std/assert'
 
-describe("hyperscript", () => {
-  const FIXTURES = [
-    '<div id="main"><h1>Hi</h1><span style="color: rgb(85, 85, 85);">555</span><label for="entry" class="name">Edit:</label><input id="entry" type="text" readonly=""></div>',
-    '<div id="main" refset="true" class="selected"><h1 title="hello" style="background-color: red;"><a href="/daft/public">Welcome</a></h1></div>',
-    '<div id="main"><button>Click Bound</button><button>Click Delegated</button><button>Click Listener</button></div>',
-    '<div>First</div>middle<div>Last</div>',
-    '<div id="main"><div>John R.<span>Smith</span></div></div>',
-    '<div id="main"><div name="John"><span>Smith</span></div></div>',
-    '<div id="main"><div class="a b"></div></div>'
-  ];
+const {window} = new JSDOM('<!DOCTYPE html>')
+const {document,MouseEvent} = window
+const r = runtime(window), h = hyperscript(r)
 
-  const {window} = new JSDOM('<!DOCTYPE html>');
-  const {document,MouseEvent} = window
-  const r = runtime(window)
-  const h = hyperscript(r);
+function assertHTML(t, e, msg) { assertEquals(t().outerHTML, e, msg) }
+function assertText(t, e, msg) { assertEquals(t().toString(), e, msg) }
 
-  describe("Simple Elements", () => {
-    const template = h("#main", [
-      h("h1", "Hi"),
-      h("span", { style: "color: #555" }, 555),
-      h("label.name", { htmlFor: "entry" }, "Edit:"),
-      h("input#entry", { type: "text", readonly: true })
-    ])();
-    assertEquals(template.outerHTML,FIXTURES[0])
+Deno.test('h', async t => {
+
+  function test(name, props, f = props) {
+    let disposer
+    return root(dispose => {
+      disposer = () => {
+        document.body.textContent = ''
+        r.clearDelegatedEvents()
+        dispose()
+      }
+      return t.step(name, f).then(disposer)
+    })
+  }
+
+  // await Promise.all([
+  //   await test('string content', () => assertHTML(h('b','A'), '<b>A</b>')),
+  //   await test('string content', () => assertHTML(h('b','A'), '<b>A</b>'))
+  // ])
+  //
+
+  // await test('blank tag', () => assertHTML(h(''), '<div></div>'))
+  await test('tag with id', () => assertHTML(h('#a'), '<div id="a"></div>'))
+  await test('tag with class', () => assertHTML(h('.a.b'), '<div class="a b"></div>'))
+  await test('no props or children', () => assertHTML(h('hr'), '<hr>'))
+  await test('string content', () => assertHTML(h('b','A'), '<b>A</b>'))
+  await test("number content", () => assertHTML(h('i',1), '<i>1</i>'))
+  await test("bigint content", () => assertHTML(h('i',2n), '<i>2</i>'))
+  await test('array content', () => assertHTML(h('i',['A',1,2n]), '<i>A12</i>'))
+  await test('signal content', () => {
+    const n = signal('A'), t = h("b", n)
+    assertHTML(t,`<b>A</b>`)
+    n('B')
+    assertHTML(t,`<b>B</b>`)
+  })
+  await test('string fragment', () => assertText(h(['A']), 'A'))
+  await test("number fragment", () => assertText(h([1]), '1'))
+  await test("bigint fragment", () => assertText(h([2n]), '2'))
+  await test('style attribute', () => assertHTML(h('hr',{style:'color:#555'}), '<hr style="color: rgb(85, 85, 85);">'))
+  await test('htmlFor attribute', () => assertHTML(h('label',{htmlFor:'a'}), '<label for="a"></label>'))
+  await test('ref attribute', () => assertHTML(h('hr', {ref: el => el.setAttribute('refset', 'true')}),'<hr refset="true">'))
+  await test('classList attribute', () => assertHTML(h('hr', {classList:()=>({selected:true})}), '<hr class="selected">'))
+
+  await test("higher-order component", () => {
+    const Hi = p => h('b',['Hi ',p.name]),
+      name = signal('An'),
+      t = h(Hi, {name:()=>name})()
+    assertHTML(t,`<b>Hi An</b>`)
+    name('Yi')
+    assertHTML(t,`<b>Hi Yi</b>`)
   });
 
-  describe("Reactive Content", () => {
-    const name = signal('Alice')
-    const template = h("div", ["Hello ",name]);
-    assertEquals(template().outerHTML,`<div>Hello Alice</div>`)
-    name('Bob')
-    assertEquals(template().outerHTML,`<div>Hello Bob</div>`)
-  });
+  // await test("onclick", () => {
+  //   const i = 0
+  //   console.log(h("button", {onclick:()=>i+=1})())//.firstChild.dispatchEvent(new MouseEvent('click',{bubbles:true}))
+  //   assertEquals(i, 1)
+  // })
 
-  describe("Component without props or children", () => {
-    assertEquals(h('hr')().outerHTML,`<hr>`)
-  });
+  // await test("onClick", () => {
+  //   const i = 0
+  //     (h("button", {onClick:()=>i+=1})()).firstChild.dispatchEvent(new MouseEvent('click',{bubbles:true}))
+  //   assertEquals(i, 1)
+  // })
 
-  describe("Element with single child ", () => {
-    const name = signal('Bob')
-    assertEquals(h('b',[name])().outerHTML,`<b>Bob</b>`)
-    assertEquals(h('b',[()=>name])().outerHTML,`<b>Bob</b>`)
-    assertEquals(h('b',name())().outerHTML,`<b>Bob</b>`)
-    // assertEquals(h('b',()=>name())().outerHTML,`<b>Bob</b>`)
-  });
-
-  // describe("Custom Component", () => {
-  //   function Greeting(props) {
-  //     return h("div", ["Hello ",props.name]);
-  //   }
-  //   const name = signal('Alice')
-  //   const template = h(Greeting, {name:'Alice'});
-  //   assertEquals(template()(),`<div>Hello Alice</div>`)
-  //   name('Bob')
-  //   assertEquals(template().outerHTML,`<div>Hello Bob</div>`)
-  // });
-
-
-//   describe("Attribute Expressions", () => {
-//     const selected = signal(true),
-//       welcoming = signal("hello");
-//     let link;
-
-//     S.root(() => {
-//       const template = h( "#main", {
-//           classList: () => ({ selected: selected() }),
-//           ref: el => {el.setAttribute("refset", "true");}
-//         },
-//         h("h1",{title: welcoming,style: () => ({ "background-color": "red" })},
-//           h("a", { href: "/", ref: r => (link = r) }, "Welcome")
-//         )
-//       )();
-//       assertEquals(template.outerHTML,FIXTURES[1])
-//     });
-//   });
-
-//   describe("Event Expressions", () => {
-//     const exec = {};
-
-//     const template = h("#main", [
-//       h("button", { onclick: () => (exec.bound = true) }, "Click Bound"),
-//       h("button", { onClick: () => (exec.delegated = true) }, "Click Delegated"),
-//       h("button", { "on:click": () => (exec.listener = true) }, "Click Listener")
-//     ])();
-//     assertEquals(template.outerHTML,FIXTURES[2])
-//     document.body.appendChild(template);
-//     let event = new MouseEvent("click", { bubbles: true });
-//     template.firstChild.dispatchEvent(event);
-//     event = new MouseEvent("click", { bubbles: true });
-//     template.firstChild.nextSibling.dispatchEvent(event);
-//     event = new MouseEvent("click", { bubbles: true });
-//     template.firstChild.nextSibling.nextSibling.dispatchEvent(event);
-
-//     assert(exec.bound)
-//     assert(exec.delegated)
-//     assert(exec.listener)
-//     document.body.textContent = "";
-//     r.clearDelegatedEvents();
-//   });
+  // await test("on:click", () => {
+  //   const i = 0
+  //   h("button", {'on:click':()=>i+=1})().firstChild.dispatchEvent(new MouseEvent('click',{bubbles:true}))
+  //   assertEquals(i, 1)
+  // })
 
 //   describe("Fragments", () => {
 //     const inserted = signal("middle");
