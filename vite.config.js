75b8c7e4211956b61a439a4a22fac53f5e769098
wrnrlr <werner@alman.ax>
2024-10-24 21:56:46 +0200

    typedoc plugin for vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,23 @@
 import * as path from '@std/path'
+import * as fs from '@std/fs'
+import {marked} from 'marked'
 import {defineConfig} from 'vite'
+import { Application, TSConfigReader, TypeDocReader } from 'typedoc'
+
+function typedocPlugin() {
+  let _config
+  return {
+    name: 'typedoc-plugin',
+    apply: 'build',
+    configResolved(config) { _config = config },
+    async writeBundle() {
+      const config = JSON.parse(await Deno.readTextFile('./typedoc.jsonc'))
+      const app = await Application.bootstrap(config)
+      const project = await app.convert()
+      await app.generateDocs(project, config.out)
+    }
+  };
+}
 
 export default defineConfig(({ command, mode }) => {
   const root = path.join(__dirname, '')
@@ -38,6 +56,7 @@ export default defineConfig(({ command, mode }) => {
     build,
     optimizeDeps,
     assetsInclude,
-    base: mode==='production' ? '/prelude' : undefined
+    base: mode==='production' ? '/prelude' : undefined,
+    plugins: [typedocPlugin()]
   }
 })
