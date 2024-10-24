40859aaeb845dd1edc9858aad7d42ab144ec99c3
wrnrlr <werner@alman.ax>
2024-10-24 17:11:36 +0200

    vite config

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import * as path from '@std/path'
+import {defineConfig} from 'vite'
+
+export default defineConfig(({ command, mode }) => {
+  const root = path.join(__dirname, '')
+
+  const define = {}
+  let build
+  const optimizeDeps = {
+    exclude: ['dist/','public/'],
+  }
+  const assetsInclude = [
+    // 'public/example/**'
+  ]
+
+  // if (command==='serve') {
+    // build = {}
+  // } else {
+    build = {
+      lib: {
+        entry: 'src/mod.ts', formats: ['es']
+      },
+      rollupOptions: {
+        input: {
+          'index.html': 'index.html',
+          'mod.js': 'src/mod.ts',
+        },
+        output: {
+          dir: 'dist',
+        }
+      }
+    }
+  // }
+
+  return {
+    root,
+    define,
+    build,
+    optimizeDeps,
+    assetsInclude
+  }
+})
