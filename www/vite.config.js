02597f8fd120166652eab84481c12cfeba00c028
wrnrlr <werner@alman.ax>
2024-11-02 03:04:35 +0100

    fix

--- www/vite.config.js
+++ www/vite.config.js
@@ -16,7 +16,7 @@ function typedocPlugin() {
       console.log('CONFIG',config)
       config.hostedBaseUrl = 'https://wrnrlr.github.io/prelude'
       config.useHostedBaseUrlForAbsoluteLinks = true
-      config.out = path.join(__dirname, './public/docs')
+      config.out = path.join(__dirname, './dist/docs')
       config.entryPoints = [path.join(__dirname, '../src/mod.ts')]
       console.log('CONFIG',config)
       const app = await Application.bootstrap(config)
