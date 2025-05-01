891fa1f3c8461753879e6a99efb4ef50ef4887a7
wrnrlr <werner@alman.ax>
2025-05-01 23:18:11 +0200

    export children helper

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,5 @@
 export type {Getter,Setter,Signal} from './reactive.ts'
-export {signal,effect,untrack,batch,memo,root,context,useContext,wrap,fuse,onMount,onCleanup,renderEffect} from './reactive.ts'
+export {signal,effect,untrack,batch,memo,root,children,context,useContext,wrap,fuse,onMount,onCleanup,renderEffect} from './reactive.ts'
 export {nbsp} from './constants.ts'
 export {List} from './list.ts'
 export {Show} from './show.ts'
